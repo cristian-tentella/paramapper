8336dca106ec3b09ef24018ed84f5bd04004d9fe
Cristian Tentella <79997224+cristian-tentella@users.noreply.github.com>
2026-06-11 21:08:11 +0200

    fix(operators): guard filters against missing object and auto_update

--- operators/filters.py
+++ operators/filters.py
@@ -4,14 +4,20 @@ import bpy  # type: ignore
 class PARAMAPPER_OT_add_filter(bpy.types.Operator):
     bl_idname = "paramapper.add_filter"
     bl_label = "Add Filter"
+    bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        context.active_object.paramapper.filters.add()
-        context.active_object.paramapper.active_filter_index = (
-            len(context.active_object.paramapper.filters) - 1
-        )
+        obj = context.active_object
+        if not obj or obj.type != "MESH":
+            self.report({"WARNING"}, "Select a Mesh Object first")
+            return {"CANCELLED"}
 
-        if context.active_object.paramapper.auto_update:
+        props = obj.paramapper
+
+        props.filters.add()
+        props.active_filter_index = len(props.filters) - 1
+
+        if props.auto_update:
             bpy.ops.paramapper.generate_nodes("INVOKE_DEFAULT")
 
         return {"FINISHED"}
@@ -20,11 +26,21 @@ class PARAMAPPER_OT_add_filter(bpy.types.Operator):
 class PARAMAPPER_OT_remove_filter(bpy.types.Operator):
     bl_idname = "paramapper.remove_filter"
     bl_label = "Remove Filter"
+    bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        props = context.active_object.paramapper
-        if len(props.filters) > 0:
+        obj = context.active_object
+        if not obj or obj.type != "MESH":
+            self.report({"WARNING"}, "Select a Mesh Object first")
+            return {"CANCELLED"}
+
+        props = obj.paramapper
+
+        if props.filters:
             props.filters.remove(props.active_filter_index)
             props.active_filter_index = max(0, props.active_filter_index - 1)
-            bpy.ops.paramapper.generate_nodes("INVOKE_DEFAULT")
+
+            if props.auto_update:
+                bpy.ops.paramapper.generate_nodes("INVOKE_DEFAULT")
+
         return {"FINISHED"}
